9fb67b2b6ad83ea9de587d8ca5072a1d3fd420c5
Travis Nelson <travis.o.nelson@gmail.com>
2018-03-25 00:39:42 -0600

    Initial checkin

---
+++ src/main/java/ColorController.java
@@ -0,0 +1,19 @@
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@EnableAutoConfiguration
+public class ColorController {
+
+	@RequestMapping("/")
+    @ResponseBody
+	JsonColorResponse home() {
+		return new JsonColorResponse("grey");
+	}
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(ColorController.class, args);
+	}
+
+}
---
+++ src/main/java/JsonColorResponse.java
@@ -0,0 +1,13 @@
+public class JsonColorResponse {
+
+    private final String color;
+
+    public JsonColorResponse(String color) {
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+}
